d4d7f382cac9defb02f195ee84b4b9e6d6dd145e
paul_abib <pabib@oui.sncf>
2021-12-07 14:43:10 +0100

    Second day 2021

---
+++ 2021/--- Day 2 Dive! ---/part two.py
@@ -0,0 +1,20 @@
+input_file = open('input.txt', 'r')
+lines = input_file.readlines()
+
+aim = 0
+depth = 0
+horizontal = 0
+
+for line in lines:
+    words = line.split()
+    match words[0]:
+        case "forward":
+            horizontal += int(words[1])
+            depth += aim*int(words[1])
+        case "up":
+            aim -= int(words[1])
+        case "down":
+            aim += int(words[1])
+    
+
+print("result is " + str(depth * horizontal))
