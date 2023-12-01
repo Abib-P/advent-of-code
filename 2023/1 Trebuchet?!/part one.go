930365c523a42f832d74c560574b540d10226f11
nainssa <paulabib17@gmail.com>
2023-12-01 20:04:52 +0100

    2023 day 1 part 1

---
+++ 2023/1 Trebuchet?!/part one.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	content, n, done := getInput()
+	if done {
+		return
+	}
+
+	// met chaque ligne du fichier dans un tableau
+	var lines []string
+	var line string
+
+	for i := 0; i < n; i++ {
+		if content[i] == '\n' {
+			lines = append(lines, line)
+			line = ""
+		} else {
+			line += string(content[i])
+		}
+	}
+
+	var numberInLine []int
+
+	for _, line := range lines {
+		var d = getNumberInLine(line)
+		var number int
+		// only first and last number are important, if only one number, use it twice
+		if len(d) == 1 {
+			number = 10*d[0] + d[0]
+		} else {
+			number = 10*d[0] + d[len(d)-1]
+		}
+		numberInLine = append(numberInLine, number)
+	}
+
+	fmt.Println(numberInLine)
+
+	var sum int
+	for _, number := range numberInLine {
+		sum += number
+	}
+
+	fmt.Println(sum)
+}
+
+func getNumberInLine(numberInLineAsString string) []int {
+	var numberInLine []int
+
+	for _, number := range numberInLineAsString {
+		numberAsInt, err := strconv.Atoi(string(number))
+		if err == nil {
+			numberInLine = append(numberInLine, numberAsInt)
+		}
+	}
+	return numberInLine
+}
+
+func getInput() ([]byte, int, bool) {
+	filePath := "input.txt"
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return nil, 0, true
+	}
+	defer file.Close()
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		fmt.Println("Error getting file info:", err)
+		return nil, 0, true
+	}
+	fileSize := fileInfo.Size()
+
+	bufferSize := int(fileSize)
+	if bufferSize < 1024 {
+		bufferSize = 1024
+	}
+
+	content := make([]byte, bufferSize)
+	n, err := file.Read(content)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return nil, 0, true
+	}
+	return content, n, false
+}
