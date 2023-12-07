15cb4e637bac9d1dd4451fbb29bb53ce5ac5496d
nainssa <paulabib17@gmail.com>
2023-12-07 22:48:34 +0100

    2023 day 7 part 1 fix

--- 2023/7 Camel Cards/part one.go
+++ 2023/7 Camel Cards/part one.go
@@ -12,23 +12,14 @@ type boat struct {
 	Distance int
 }
 
-// type de fichier :
-//32T3K 765
-//T55J5 684
-//KK677 28
-//KTJJT 220
-//QQQJA 483
-
 type hand struct {
 	Id   int
 	Hand string
 	Type int
 }
 
-// declare all possible card here
 const listCard = "23456789TJQKA"
 
-// declare const here
 const fiveOfAKind = 7
 const fourOfAKind = 6
 const fullHouse = 5
@@ -65,24 +56,6 @@ func main() {
 		hands = append(hands, newHand)
 	}
 
-	//sort hands by type, if type is the same, compare first card then second card...
-	/*for i := 0; i < len(hands); i++ {
-		for j := i + 1; j < len(hands); j++ {
-			if hands[i].Type < hands[j].Type {
-				hands[i], hands[j] = hands[j], hands[i]
-			} else if hands[i].Type == hands[j].Type {
-				for k := 0; k < 5; k++ {
-					if strings.IndexByte(listCard, hands[i].Hand[k]) < strings.IndexByte(listCard, hands[j].Hand[k]) {
-						hands[i], hands[j] = hands[j], hands[i]
-						break
-					} else if strings.IndexByte(listCard, hands[i].Hand[k]) > strings.IndexByte(listCard, hands[j].Hand[k]) {
-						break
-					}
-				}
-			}
-		}
-	}*/
-	//this but not from 7 to 1 but from 1 to 7
 	for i := 0; i < len(hands); i++ {
 		for j := i + 1; j < len(hands); j++ {
 			if hands[i].Type > hands[j].Type {
