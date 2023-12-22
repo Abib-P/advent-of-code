50bfc18e20d7531b955f5810ae25d9156567c9af
nainssa <paulabib17@gmail.com>
2023-12-22 03:40:55 +0100

    2021 day 1

--- 2021 - Python/1 Sonar Sweep/part two.py
+++ 2021 - Python/1 Sonar Sweep/part two.py
@@ -1,17 +1,18 @@
-input_file = open('input.txt', 'r')
-lines = input_file.readlines()
-
-count = 0
-three_last_number = []
-
-for line in lines:
-    if len(three_last_number) < 3:
+def sonar_sweep():
+    input_file = open('input.txt', 'r')
+    lines = input_file.readlines()
+    count = 0
+    three_last_number = []
+    for line in lines:
+        if len(three_last_number) < 3:
+            three_last_number.append(int(line))
+            continue
+        last_sum = sum(three_last_number)
+        three_last_number.pop(0)
         three_last_number.append(int(line))
-        continue
-    last_sum = sum(three_last_number)
-    three_last_number.pop(0)
-    three_last_number.append(int(line))
-    if last_sum < sum(three_last_number):
-        count += 1
+        if last_sum < sum(three_last_number):
+            count += 1
+    print("result is " + str(count))
 
-print("result is " + str(count))
+
+sonar_sweep()
