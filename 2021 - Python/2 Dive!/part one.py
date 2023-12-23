d135c0738823b86eff207845e7af19b55c57248c
nainssa <paulabib17@gmail.com>
2023-12-23 01:24:16 +0100

    2021 day 2

--- 2021 - Python/2 Dive!/part one.py
+++ 2021 - Python/2 Dive!/part one.py
@@ -1,17 +1,18 @@
-input_file = open('input.txt', 'r')
-lines = input_file.readlines()
+def dive_part_one():
+    input_file = open('input.txt', 'r')
+    lines = input_file.readlines()
+    depth = 0
+    horizontal = 0
+    for line in lines:
+        words = line.split()
+        match words[0]:
+            case "forward":
+                horizontal += int(words[1])
+            case "up":
+                depth -= int(words[1])
+            case "down":
+                depth += int(words[1])
+    print("result is " + str(depth * horizontal))
 
-depth = 0
-horizontal = 0
 
-for line in lines:
-    words = line.split()
-    match words[0]:
-        case "forward":
-            horizontal += int(words[1])
-        case "up":
-            depth -= int(words[1])
-        case "down":
-            depth += int(words[1])
-
-print("result is " + str(depth * horizontal))
+dive_part_one()
--- 2021 - Python/2 Dive!/part two.py
+++ 2021 - Python/2 Dive!/part two.py
@@ -1,19 +1,20 @@
-input_file = open('input.txt', 'r')
-lines = input_file.readlines()
+def dive_part_two():
+    input_file = open('input.txt', 'r')
+    lines = input_file.readlines()
+    aim = 0
+    depth = 0
+    horizontal = 0
+    for line in lines:
+        words = line.split()
+        match words[0]:
+            case "forward":
+                horizontal += int(words[1])
+                depth += aim * int(words[1])
+            case "up":
+                aim -= int(words[1])
+            case "down":
+                aim += int(words[1])
+    print("result is " + str(depth * horizontal))
 
-aim = 0
-depth = 0
-horizontal = 0
 
-for line in lines:
-    words = line.split()
-    match words[0]:
-        case "forward":
-            horizontal += int(words[1])
-            depth += aim * int(words[1])
-        case "up":
-            aim -= int(words[1])
-        case "down":
-            aim += int(words[1])
-
-print("result is " + str(depth * horizontal))
+dive_part_two()
