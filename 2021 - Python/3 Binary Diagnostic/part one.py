297d3346f15a5d4461c90cb8ba53ba3ade2959fc
nainssa <paulabib17@gmail.com>
2023-12-25 03:40:26 +0100

    Export into method 2021 - Day 3

--- 2021 - Python/3 Binary Diagnostic/part one.py
+++ 2021 - Python/3 Binary Diagnostic/part one.py
@@ -1,33 +1,30 @@
-input_file = open('input.txt', 'r')
-lines = input_file.readlines()
+def binary_diagnostic_part_one():
+    input_file = open('input.txt', 'r')
+    lines = input_file.readlines()
+    number_of_bit_by_line = len(lines[0]) - 1
+    gamma_rate = []
+    for i in range(number_of_bit_by_line):
+        count_0 = 0
+        count_1 = 0
+        for line in lines:
+            match line[i]:
+                case '0':
+                    count_0 += 1
+                case '1':
+                    count_1 += 1
+        if count_0 > count_1:
+            gamma_rate.append(0)
+        else:
+            gamma_rate.append(1)
+    epsilon_rate = [i ^ 1 for i in gamma_rate]
+    gamma_rate = gamma_rate[::-1]
+    epsilon_rate = epsilon_rate[::-1]
+    gamma_result = 0
+    epsilon_result = 0
+    for i in range(number_of_bit_by_line):
+        gamma_result += gamma_rate[i] * pow(2, i)
+        epsilon_result += epsilon_rate[i] * pow(2, i)
+    print("result is " + str(gamma_result * epsilon_result))
 
-number_of_bit_by_line = len(lines[0]) - 1
-gamma_rate = []
 
-for i in range(number_of_bit_by_line):
-    count_0 = 0
-    count_1 = 0
-    for line in lines:
-        match line[i]:
-            case '0':
-                count_0 += 1
-            case '1':
-                count_1 += 1
-    if count_0 > count_1:
-        gamma_rate.append(0)
-    else:
-        gamma_rate.append(1)
-
-epsilon_rate = [i ^ 1 for i in gamma_rate]
-
-gamma_rate = gamma_rate[::-1]
-epsilon_rate = epsilon_rate[::-1]
-
-gamma_result = 0
-epsilon_result = 0
-
-for i in range(number_of_bit_by_line):
-    gamma_result += gamma_rate[i] * pow(2, i)
-    epsilon_result += epsilon_rate[i] * pow(2, i)
-
-print("result is " + str(gamma_result * epsilon_result))
+binary_diagnostic_part_one()
--- 2021 - Python/3 Binary Diagnostic/part two.py
+++ 2021 - Python/3 Binary Diagnostic/part two.py
@@ -1,69 +1,66 @@
-input_file = open('input.txt', 'r')
-oxygen_generator_rating_lines = input_file.readlines()
-CO2_scrubber_rating_lines = oxygen_generator_rating_lines
-
-number_of_bit_by_line = len(oxygen_generator_rating_lines[0]) - 1
-
-for i in range(number_of_bit_by_line):
-    count_0 = 0
-    count_1 = 0
-    for line in oxygen_generator_rating_lines:
-        match line[i]:
-            case '0':
-                count_0 += 1
-            case '1':
-                count_1 += 1
-    if count_0 > count_1:
-        new_lines = []
+def binary_diagnostic_part_two():
+    input_file = open('input.txt', 'r')
+    oxygen_generator_rating_lines = input_file.readlines()
+    CO2_scrubber_rating_lines = oxygen_generator_rating_lines
+    number_of_bit_by_line = len(oxygen_generator_rating_lines[0]) - 1
+    for i in range(number_of_bit_by_line):
+        count_0 = 0
+        count_1 = 0
         for line in oxygen_generator_rating_lines:
             match line[i]:
                 case '0':
-                    new_lines.append(line)
-        oxygen_generator_rating_lines = new_lines
-    else:
-        new_lines = []
-        for line in oxygen_generator_rating_lines:
-            match line[i]:
+                    count_0 += 1
                 case '1':
-                    new_lines.append(line)
-        oxygen_generator_rating_lines = new_lines
-    if (len(oxygen_generator_rating_lines) == 1):
-        break
-
-for i in range(number_of_bit_by_line):
-    count_0 = 0
-    count_1 = 0
-    for line in CO2_scrubber_rating_lines:
-        match line[i]:
-            case '0':
-                count_0 += 1
-            case '1':
-                count_1 += 1
-    if count_0 > count_1:
-        new_lines = []
-        for line in CO2_scrubber_rating_lines:
-            match line[i]:
-                case '1':
-                    new_lines.append(line)
-        CO2_scrubber_rating_lines = new_lines
-    else:
-        new_lines = []
+                    count_1 += 1
+        if count_0 > count_1:
+            new_lines = []
+            for line in oxygen_generator_rating_lines:
+                match line[i]:
+                    case '0':
+                        new_lines.append(line)
+            oxygen_generator_rating_lines = new_lines
+        else:
+            new_lines = []
+            for line in oxygen_generator_rating_lines:
+                match line[i]:
+                    case '1':
+                        new_lines.append(line)
+            oxygen_generator_rating_lines = new_lines
+        if (len(oxygen_generator_rating_lines) == 1):
+            break
+    for i in range(number_of_bit_by_line):
+        count_0 = 0
+        count_1 = 0
         for line in CO2_scrubber_rating_lines:
             match line[i]:
                 case '0':
-                    new_lines.append(line)
-        CO2_scrubber_rating_lines = new_lines
-    if (len(CO2_scrubber_rating_lines) == 1):
-        break
+                    count_0 += 1
+                case '1':
+                    count_1 += 1
+        if count_0 > count_1:
+            new_lines = []
+            for line in CO2_scrubber_rating_lines:
+                match line[i]:
+                    case '1':
+                        new_lines.append(line)
+            CO2_scrubber_rating_lines = new_lines
+        else:
+            new_lines = []
+            for line in CO2_scrubber_rating_lines:
+                match line[i]:
+                    case '0':
+                        new_lines.append(line)
+            CO2_scrubber_rating_lines = new_lines
+        if (len(CO2_scrubber_rating_lines) == 1):
+            break
+    oxygen_generator_rating = oxygen_generator_rating_lines[0][::-1].replace('\n', '')
+    CO2_scrubber_rating = CO2_scrubber_rating_lines[0][::-1].replace('\n', '')
+    oxygen_generator_result = 0
+    CO2_scrubber_result = 0
+    for i in range(number_of_bit_by_line):
+        oxygen_generator_result += int(oxygen_generator_rating[i]) * pow(2, i)
+        CO2_scrubber_result += int(CO2_scrubber_rating[i]) * pow(2, i)
+    print("result is " + str(oxygen_generator_result * CO2_scrubber_result))
 
-oxygen_generator_rating = oxygen_generator_rating_lines[0][::-1].replace('\n', '')
-CO2_scrubber_rating = CO2_scrubber_rating_lines[0][::-1].replace('\n', '')
 
-oxygen_generator_result = 0
-CO2_scrubber_result = 0
-
-for i in range(number_of_bit_by_line):
-    oxygen_generator_result += int(oxygen_generator_rating[i]) * pow(2, i)
-    CO2_scrubber_result += int(CO2_scrubber_rating[i]) * pow(2, i)
-
-print("result is " + str(oxygen_generator_result * CO2_scrubber_result))
+binary_diagnostic_part_two()
