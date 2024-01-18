64e2ce4fc33975fbfd6f56e99bb9a9499eb5d0b0
nainssa <paulabib17@gmail.com>
2024-01-18 18:59:35 +0100

    2021 - reformat main file

--- 2021 - Python/main.py
+++ 2021 - Python/main.py
@@ -1,9 +1,10 @@
-#from  file 1_sonar_sweep import sonar_sweep_part_one
 sonar_sweep = __import__('1_Sonar_Sweep')
+
 
 def main():
     sonar_sweep.part_one("./1_Sonar_Sweep/input.txt")
     sonar_sweep.part_two("./1_Sonar_Sweep/input.txt")
+
 
 if __name__ == "__main__":
     main()
