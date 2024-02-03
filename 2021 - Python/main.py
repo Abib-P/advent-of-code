935b2946c2bfa06832e7a048760328e9ca4070cb
nainssa <paulabib17@gmail.com>
2024-02-03 03:00:11 +0100

    2021 -

--- 2021 - Python/main.py
+++ 2021 - Python/main.py
@@ -1,5 +1,6 @@
 sonar_sweep = __import__('1_Sonar_Sweep')
 dive = __import__('2_Dive')
+binary_diagnostic = __import__('3_Binary_Diagnostic')
 
 
 def main():
@@ -7,6 +8,8 @@ def main():
     sonar_sweep.part_two("./1_Sonar_Sweep/input.txt")
     dive.part_one()
     dive.part_two()
+    binary_diagnostic.part_one()
+    binary_diagnostic.part_two()
 
 
 if __name__ == "__main__":
