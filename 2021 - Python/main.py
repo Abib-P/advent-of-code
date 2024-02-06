4def7514bf6daba5bd8513fa3b1fc942c50bc30e
nainssa <paulabib17@gmail.com>
2024-02-06 01:07:50 +0100

    2021 - update main

--- 2021 - Python/main.py
+++ 2021 - Python/main.py
@@ -2,6 +2,9 @@ sonar_sweep = __import__('1_Sonar_Sweep')
 dive = __import__('2_Dive')
 binary_diagnostic = __import__('3_Binary_Diagnostic')
 giant_squid = __import__('4_Giant_Squid')
+hydrothermal_venture = __import__('5_Hydrothermal_Venture')
+lanternfish = __import__('6_Lanternfish')
+the_treachery_of_whales = __import__('7_The_Treachery_of_Whales')
 
 
 def main():
@@ -13,6 +16,12 @@ def main():
     binary_diagnostic.part_two("./3_Binary_Diagnostic/input.txt")
     giant_squid.part_one("./4_Giant_Squid/input.txt")
     giant_squid.part_two("./4_Giant_Squid/input.txt")
+    hydrothermal_venture.part_one("./5_Hydrothermal_Venture/input.txt")
+    hydrothermal_venture.part_two("./5_Hydrothermal_Venture/input.txt")
+    lanternfish.part_one("./6_Lanternfish/input.txt")
+    lanternfish.part_two("./6_Lanternfish/input.txt")
+    the_treachery_of_whales.part_one("./7_The_Treachery_of_Whales/input.txt")
+
 
 
 if __name__ == "__main__":
